3e2a36093ce59d253b13839214f4344ef286d0b0
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-27 21:07:01 +0100

    add hashes

--- array.rb
+++ array.rb
@@ -2,3 +2,15 @@
 s = [["ham", "swiss"], ["turkey", "cheddar"], ["roast beef", "gruyere"]]
 
 s.each { |sub_array| sub_array.each {|element| puts element }}
+
+#iterating over hashes
+secret_identities = {
+  "The Batman" => "Bruce Wayne",
+  "Superman" => "Clark Kent",
+  "Wonder Woman" => "Diana Prince",
+  "Freakazoid" => "Dexter Douglas"
+}
+  
+secret_identities.each {|personagem, ator|
+puts "#{personagem}: #{ator}"
+}
