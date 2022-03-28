b13f4933264dfab13c7ddf7a142342d3fdcf707b
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-28 22:29:14 +0100

    add sort!

---
+++ blocks.rb
@@ -0,0 +1,5 @@
+books = ["Charlie and the Chocolate Factory", "War and Peace", "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+
+# To sort our books in ascending order, in-place
+books.sort! { |firstBook, secondBook| secondBook <=> firstBook }
+puts books
