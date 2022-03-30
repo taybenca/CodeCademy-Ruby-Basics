f86d026746297c4451acd497a49fd2c861e0037a
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-30 22:10:44 +0100

    add case

--- variables.rb
+++ variables.rb
@@ -5,3 +5,10 @@ puts name
 puts sum  = 13+379
 puts product = 923*15
 puts quotient = 13209/17
+
+case language
+    when "JS" then puts "Websites!"
+    when "Python" then puts "Science!"
+    when "Ruby" then puts "Web apps!"
+else puts "I don't know!"
+end     
