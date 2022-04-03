10ab24fa1a949a83429748893951290315e75e58
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-04-03 16:04:13 +0100

    add gsub function

---
+++ pangram.rb
@@ -0,0 +1,28 @@
+=begin
+name = "tayanne"
+
+letras_separadas = name.split("") #convertendo em array de caracteres
+frequencia = Hash.new (0)
+
+letras_separadas.each { |letra| puts letra }
+letras_separadas.each { |a| frequencia[a] +=1}
+print frequencia
+=end
+
+def count_char (string)
+    
+    characters = string.gsub(/[^a-zA-Z]/, '').downcase.split("")
+    freq = Hash.new(0)
+    characters.each { |char| 
+        freq[char] +=1 
+    }
+        return freq.length == 26
+end
+
+puts count_char("The quick brown fox jumps over the lazy dog")
+puts "Hello world 123".gsub(/[^a-zA-Z0-9]/, '') 
+
+#other solution
+def panagram?(string)
+    ('a'..'z').all? { |x| string.downcase.include? (x) } 
+  end
