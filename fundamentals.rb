03ebc50390543a4a690c7e5d00f78a6b56f9e128
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-04-03 22:51:21 +0100

    add media codewars

--- fundamentals.rb
+++ fundamentals.rb
@@ -4,12 +4,22 @@ end
 
 puts multiply(2,3) == 6
 
+
+
 def string_to_array(string)
     string.split(" ")
 end
 puts string_to_array("1 2 3") == ["1", "2", "3"]
 
 
+def swap(string)
+    string.swapcase
+end
+
+puts swap("Tay")
+
+
+=begin
 --------------#consertar isso - encontrar o proximo numero nao conseccutivo de uma array
 def first_non_consecutive(arr)
     result = 0
@@ -21,3 +31,15 @@ def first_non_consecutive(arr)
   end
 
   puts first_non_consecutive([2,3,4,7,8])
+=end
+
+  #soma tudo e divide pelo numero 
+  def media (array)      
+    result = 0
+    for i in 0..array.length-1     #somar todos os valores da array e dividir pelo numero de elementos da array
+        result = result + i
+    end
+    new_result = result/array.length
+    return new_result
+  end
+puts ([1,2,3,4,5])
