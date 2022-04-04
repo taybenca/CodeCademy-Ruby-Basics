b35fcb70ce005c9dd65a293e6fd1f59e779d8a15
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-04-04 21:28:26 +0100

    add rps

--- fundamentals.rb
+++ fundamentals.rb
@@ -43,3 +43,20 @@ def first_non_consecutive(arr)
     return new_result
   end
 puts ([1,2,3,4,5])
+
+
+## return which player won. In case of a draw, return draw.
+
+def rps(p1, p2)
+  if p1 == p2
+    return "Draw!"
+  elsif p1 == "scissors" && p2 == "paper" || p1 == "rock" && p2 == "scissors" || p1 == "paper" && p2 == "rock"
+    return "Player 1 won!"
+  else
+    return "Player 2 won!"
+  end
+
+end
+puts rps("scissors", "paper") == "Player 1 won!"
+puts rps("scissors", "rock") == "Player 2 won!"
+puts rps("paper", "paper") == "Draw!"
